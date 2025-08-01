59ea52b962fd95641f4c8a1dfcac28141f0af1b5
Corbin Reak <corbinreak@gmail.com>
2025-08-01 17:52:53 -0600

    added a animation to my main passage.

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,17 +16,26 @@ function About({ darkMode, setDarkMode }) {
         marginTop: '68px'
     }
 
-
-    
+    const lines = [
+        "This portfolio showcases some of my projects and experiences in the field.",
+        "I have a passion for software development and I am always eager to learn new technologies and improve my skills.",
+        <>Hello! My name is <span><Link to="/about-extended" className="about-link">Corbin Reak</Link></span>, and I am a Computer Science student at the University of Phoenix.</>
+    ].reverse();
 
     return (
-        <>
-            <article style={articleStyle}>
-                <p>
-                    Hello! My name is <span ><Link to="/about-extended" className='about-link'>Corbin Reak</Link></span>, and I am a Computer Science student at the University of Phoenix. I have a passion for software development and I am always eager to learn new technologies and improve my skills. This portfolio showcases some of my projects and experiences in the field.
-                </p>
-            </article>
-        </>
+        <article style={articleStyle}>
+            <div>
+                {lines.map((line, idx) => (
+                    <div
+                        key={idx}
+                        className="animated-line"
+                        style={{ animationDelay: `${idx * 0.4}s` }}
+                    >
+                        {line}
+                    </div>
+                ))}
+            </div>
+        </article>
     )
 }
 
